f4ae5970e8dfe1fb34548305b281aac60fae79f1
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2021-12-15 20:56:11 -0300

    Agregar archivos de proyecto.

---
+++ Del 1 al 100 en cinco/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Del_1_al_100_en_cinco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1 = 1;
+            int Num2 = 100;
+            while(Num1 <= Num2)
+            {
+                Console.WriteLine("{0}", Num1);
+                if (Num1 == 1)
+                {
+                    Num1 = Num1 + 4;
+                }
+                else
+                {
+                    Num1 = Num1 + 5;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
